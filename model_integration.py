d82caa731a209c7e68ac18cc9f9341ab091fcbc8
felix-geffrault <felix.geffrault@gmail.com>
2021-12-02 10:33:42 +0100

    Update

--- model_integration.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel('data/crimes-et-delits-enregistres-par-les-services-de-gendarmerie-et-de-police-depuis-2012.xlsx', sheet_name="Services GN 2012", skiprows=1)
-
-data = df
